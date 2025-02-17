37391eeee2b2b493035a1ad6c4ce2355ce1271b8
cjihrig <cjihrig@gmail.com>
2025-02-16 23:00:55 -0500

    controller-runtime: add types for reconcile.js

--- controller-runtime/lib/reconcile.d.ts
+++ controller-runtime/lib/reconcile.d.ts
@@ -1,15 +1,47 @@
+/**
+ * @typedef {import('./context.js').Context} Context
+ */
+/**
+ * Request contains the information necessary to reconcile a Kubernetes object.
+ * This includes the information to uniquely identify the object. It does not
+ * contain information about any specific Event or the object contents itself.
+ */
 export class Request extends NamespacedName {
 }
+/**
+ * Result contains the result of a Reconciler invocation.
+ */
 export class Result {
+    /**
+     * Construct a Result.
+     * @param {any} requeue - Whether or not to requeue a reconciliation.
+     */
     constructor(requeue: any);
     requeue: boolean;
     requeueAfter: number;
 }
+/**
+ * TerminalError is an error that will not be retried.
+ */
 export class TerminalError extends Error {
+    /**
+     * Construct a TerminalError.
+     * @param {any} cause - The underlying cause of the TerminalError.
+     */
     constructor(cause: any);
 }
+/**
+ * Reconciler provides an interface for implementing reconciliation.
+ */
 export class Reconciler {
-    reconcile(context: any, request: any): Promise<void>;
+    /**
+     * reconcile() implementations compare the desired state of an object as
+     * specified by the user against the actual cluster state, and then performs
+     * operations to make the actual cluster state reflect the desired state.
+     * @param {Context} context - The context of the reconciliation.
+     * @param {Request} request - The requested resource information.
+     */
+    reconcile(context: Context, request: Request): Promise<void>;
 }
 declare namespace _default {
     export { Reconciler };
@@ -18,4 +50,5 @@ declare namespace _default {
     export { TerminalError };
 }
 export default _default;
+export type Context = import("./context.js").Context;
 import { NamespacedName } from './apimachinery/types.js';
--- controller-runtime/lib/reconcile.js
+++ controller-runtime/lib/reconcile.js
@@ -1,8 +1,24 @@
 import { NamespacedName } from './apimachinery/types.js';
 
+/**
+ * @typedef {import('./context.js').Context} Context
+ */
+
+/**
+ * Request contains the information necessary to reconcile a Kubernetes object.
+ * This includes the information to uniquely identify the object. It does not
+ * contain information about any specific Event or the object contents itself.
+ */
 export class Request extends NamespacedName {}
 
+/**
+ * Result contains the result of a Reconciler invocation.
+ */
 export class Result {
+  /**
+   * Construct a Result.
+   * @param {any} requeue - Whether or not to requeue a reconciliation.
+   */
   constructor(requeue) {
     if (typeof requeue === 'number') {
       this.requeue = true;
@@ -14,13 +30,30 @@ export class Result {
   }
 }
 
+/**
+ * TerminalError is an error that will not be retried.
+ */
 export class TerminalError extends Error {
+  /**
+   * Construct a TerminalError.
+   * @param {any} cause - The underlying cause of the TerminalError.
+   */
   constructor(cause) {
     super('terminal error', { cause });
   }
 }
 
+/**
+ * Reconciler provides an interface for implementing reconciliation.
+ */
 export class Reconciler {
+  /**
+   * reconcile() implementations compare the desired state of an object as
+   * specified by the user against the actual cluster state, and then performs
+   * operations to make the actual cluster state reflect the desired state.
+   * @param {Context} context - The context of the reconciliation.
+   * @param {Request} request - The requested resource information.
+   */
   // eslint-disable-next-line class-methods-use-this, require-await
   async reconcile(context, request) {
     throw new Error('unimplemented reconcile()', { cause: request });
